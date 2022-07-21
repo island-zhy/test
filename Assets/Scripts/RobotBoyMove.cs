fd3b7984c272584b9d7b4dd533b4a02df14cf8ce
Zhang Haoyu <2395390229@qq.com>
2022-07-21 13:05:15 +0800

    update some sprites
    update some sprites

---
+++ Assets/Scripts/RobotBoyMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotBoyMove : MonoBehaviour {
+
+  // Use this for initialization
+  void Start () {
+	  //this is a commit
+
+  }
+	
+  // Update is called once per frame
+  void Update () {
+		
+  }
+}

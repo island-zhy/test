5e3c48959fd7fbfa03fffe4da644aa4653f7433c
Zhang Haoyu <2395390229@qq.com>
2022-07-21 13:29:07 +0800

    Update RobotBoyMove.cs
    Update RobotBoyMove.cs

--- Assets/Scripts/RobotBoyMove.cs
+++ Assets/Scripts/RobotBoyMove.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class RobotBoyMove : MonoBehaviour {
 
-  // Use this for initialization
-  void Start () {
-	  //this is a commit
+  [SerializeField]
+  private float _jump_Force = 400f;
 
+  private bool m_IfGround;
+  private Rigidbody m_rigidbody;
+
+  private void _Jump() {
+    m_rigidbody.AddForce(new Vector3(0, _jump_Force, 0));
+    m_IfGround = false;
   }
-	
-  // Update is called once per frame
-  void Update () {
-		
+
+  void Start() {
+    m_IfGround = false;
+    m_rigidbody = GetComponent<Rigidbody>();
+  }
+
+  void FixedUpdate() {
+
+    if ((Input.GetAxis("Jump_k") != 0 || Input.GetAxis("Jump_j1") != 0) && !m_IfGround) {
+      _Jump();
+    }
   }
 }

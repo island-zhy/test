d44b8143285e85e15f7173acfbbeebc4cf057803
Zhang Haoyu <2395390229@qq.com>
2022-07-21 14:35:08 +0800

    update RobotMove
    update RobotMove

--- Assets/Scripts/RobotBoyMove.cs
+++ Assets/Scripts/RobotBoyMove.cs
@@ -6,25 +6,83 @@ using UnityEngine;
 public class RobotBoyMove : MonoBehaviour {
 
   [SerializeField]
-  private float _jump_Force = 400f;
+  private float _jumpForce = 400f;
+  [SerializeField]
+  private int _playerID;
+  [SerializeField]
+  private float _moveSpeed = 4f;
 
-  private bool m_IfGround;
-  private Rigidbody m_rigidbody;
+  private const float m_groundedRadius = 0.2f;
+  private bool m_ifGround;
+  private Transform m_groundTransform;
+  private bool m_faceToRight = true;
+  private Rigidbody2D m_rigidbody;
 
   private void _Jump() {
-    m_rigidbody.AddForce(new Vector3(0, _jump_Force, 0));
-    m_IfGround = false;
+    m_rigidbody.AddForce(new Vector2(0, _jumpForce));
+    m_ifGround = false;
+  }
+
+  private void _Flip() {
+    this.transform.localScale = new Vector3(-transform.localScale.x , transform.localScale.y, transform.localScale.z);
+    m_faceToRight = !m_faceToRight;
   }
 
   void Start() {
-    m_IfGround = false;
-    m_rigidbody = GetComponent<Rigidbody>();
+    m_groundTransform = transform.Find("GroundCheck");
+    m_rigidbody = GetComponent<Rigidbody2D>();
   }
 
   void FixedUpdate() {
 
-    if ((Input.GetAxis("Jump_k") != 0 || Input.GetAxis("Jump_j1") != 0) && !m_IfGround) {
+    //判断人物是否在地面上
+    m_ifGround = false;
+    Collider2D[] colliders = Physics2D.OverlapCircleAll(m_groundTransform.position, m_groundedRadius);
+    for (int i = 0; i < colliders.Length; i++) {
+      if (colliders[i].gameObject != gameObject) {
+        m_ifGround = true;
+      }
+    }
+
+    // 判断人物id是player1还是player2进行跳跃
+    if ( ( (_playerID == 1 && (Input.GetAxis("jump_k1") != 0 || Input.GetAxis("jump_j1") != 0)) ||
+           (_playerID == 2 && (Input.GetAxis("jump_k2") != 0 || Input.GetAxis("jump_j2") != 0)) )  &&
+          m_ifGround) {
       _Jump();
     }
+
+    if (m_ifGround) {
+      if (_playerID == 1) {
+        float _move = Input.GetAxisRaw("Horizontal_joy1") + Input.GetAxisRaw("Horizontal_k1");
+        if (_move > 0.4) {
+          m_rigidbody.velocity = new Vector2(_moveSpeed, m_rigidbody.velocity.y);
+        } else if (_move < -0.4) {
+          m_rigidbody.velocity = new Vector2(-_moveSpeed, m_rigidbody.velocity.y);
+        } else {
+          m_rigidbody.velocity = new Vector2(0, m_rigidbody.velocity.y);
+        }
+        if (_move > 0.4 && !m_faceToRight) {
+          _Flip();
+        } else if (_move < -0.4 && m_faceToRight) {
+          _Flip();
+        }
+      } else if (_playerID == 2) {
+        float _move = Input.GetAxisRaw("Horizontal_joy2") + Input.GetAxisRaw("Horizontal_k2");
+        if (_move > 0.4) {
+          m_rigidbody.velocity = new Vector2(_moveSpeed, m_rigidbody.velocity.y);
+        } else if (_move < -0.4) {
+          m_rigidbody.velocity = new Vector2(-_moveSpeed, m_rigidbody.velocity.y);
+        } else {
+          m_rigidbody.velocity = new Vector2(0, m_rigidbody.velocity.y);
+        }
+        if (_move > 0.4 && !m_faceToRight) {
+          _Flip();
+        } else if (_move < -0.4 && m_faceToRight) {
+          _Flip();
+        }
+      }
+    }
   }
+  
+
 }

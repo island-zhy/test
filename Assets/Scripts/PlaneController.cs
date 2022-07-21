0dfd198b59c6a305a849fb600ac3b788b2d41516
Zhang Haoyu <2395390229@qq.com>
2022-07-21 17:35:38 +0800

    update worldflip
    update worldflip's method

---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour {
+
+	public void BeRushed() {
+		this.transform.position -= new Vector3(0, 1, 0);
+	}
+
+}
--- Assets/Scripts/RobotBoyMove.cs
+++ Assets/Scripts/RobotBoyMove.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿///<summary>
+///定义机器人运动跳跃等
+/// </summary>
+using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
@@ -11,12 +14,24 @@ public class RobotBoyMove : MonoBehaviour {
   private int _playerID;
   [SerializeField]
   private float _moveSpeed = 4f;
+  [SerializeField]
+  private float _rushSpeedMin = -6f;
 
   private const float m_groundedRadius = 0.2f;
   private bool m_ifGround;
-  private Transform m_groundTransform;
+  private bool m_ifRushGround = true;
+  private bool m_afterWorldFlip = false;
   private bool m_faceToRight = true;
+  private Transform m_groundTransform;
   private Rigidbody2D m_rigidbody;
+
+  /// <summary>
+  /// 跟随世界进行翻转
+  /// </summary>
+  public void FollowWorldFlip() {
+    this.transform.Rotate(180f, 0, 0);
+    m_afterWorldFlip=true;
+  }
 
   private void _Jump() {
     m_rigidbody.AddForce(new Vector2(0, _jumpForce));
@@ -28,6 +43,27 @@ public class RobotBoyMove : MonoBehaviour {
     m_faceToRight = !m_faceToRight;
   }
 
+  /// <summary>
+  /// 判断是否人物滞空后是否落到地上，以及是否把地板冲了
+  /// </summary>
+  private void _IfOnGround() {
+    m_ifGround = false;
+    Collider2D[] colliders = Physics2D.OverlapCircleAll(m_groundTransform.position, m_groundedRadius);
+    for (int i = 0; i < colliders.Length; i++) {
+      if (colliders[i].gameObject != gameObject) {
+        m_ifGround = true;
+        if (m_ifRushGround && m_rigidbody.velocity.y < _rushSpeedMin && colliders[i].gameObject.name.Equals("Plane")) {
+          m_ifRushGround = false;
+          if (!m_afterWorldFlip) {
+            colliders[i].gameObject.GetComponent<PlaneController>().BeRushed();
+          } else {
+            m_afterWorldFlip = false;
+          }
+        }
+      }
+    }
+  }
+
   void Start() {
     m_groundTransform = transform.Find("GroundCheck");
     m_rigidbody = GetComponent<Rigidbody2D>();
@@ -36,24 +72,27 @@ public class RobotBoyMove : MonoBehaviour {
   void FixedUpdate() {
 
     //判断人物是否在地面上
-    m_ifGround = false;
-    Collider2D[] colliders = Physics2D.OverlapCircleAll(m_groundTransform.position, m_groundedRadius);
-    for (int i = 0; i < colliders.Length; i++) {
-      if (colliders[i].gameObject != gameObject) {
-        m_ifGround = true;
-      }
+    if (m_rigidbody.velocity.y < 0) {
+      _IfOnGround();
     }
 
-    // 判断人物id是player1还是player2进行跳跃
-    if ( ( (_playerID == 1 && (Input.GetAxis("jump_k1") != 0 || Input.GetAxis("jump_j1") != 0)) ||
-           (_playerID == 2 && (Input.GetAxis("jump_k2") != 0 || Input.GetAxis("jump_j2") != 0)) )  &&
+    //判断人物有没有资格去冲地板
+    //用来防止连续冲地板
+    if (m_rigidbody.velocity.y == 0) {
+      m_ifRushGround = true;
+    }
+
+    // jump
+    if ( ( (_playerID == 1 && (Input.GetAxis("jump_k1") != 0 ) ) ||
+           (_playerID == 2 && (Input.GetAxis("jump_k2") != 0 ) ) )  &&
           m_ifGround) {
       _Jump();
     }
 
+    //move
     if (m_ifGround) {
       if (_playerID == 1) {
-        float _move = Input.GetAxisRaw("Horizontal_joy1") + Input.GetAxisRaw("Horizontal_k1");
+        float _move = Input.GetAxisRaw("Horizontal");
         if (_move > 0.4) {
           m_rigidbody.velocity = new Vector2(_moveSpeed, m_rigidbody.velocity.y);
         } else if (_move < -0.4) {
@@ -67,7 +106,7 @@ public class RobotBoyMove : MonoBehaviour {
           _Flip();
         }
       } else if (_playerID == 2) {
-        float _move = Input.GetAxisRaw("Horizontal_joy2") + Input.GetAxisRaw("Horizontal_k2");
+        float _move = Input.GetAxisRaw("Horizontal2");
         if (_move > 0.4) {
           m_rigidbody.velocity = new Vector2(_moveSpeed, m_rigidbody.velocity.y);
         } else if (_move < -0.4) {
@@ -82,6 +121,7 @@ public class RobotBoyMove : MonoBehaviour {
         }
       }
     }
+
   }
   
 

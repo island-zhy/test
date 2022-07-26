58e1fcc61bac5566dde4d33132924c56a5eb72bb
Zhang Haoyu <2395390229@qq.com>
2022-07-26 16:27:27 +0800

    upload 2.5d
    upload 2.5d

---
+++ Assets/Script/FacingCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacingCamera : MonoBehaviour {
+  private List<Transform> childs;
+  void Start() {
+    childs = new List<Transform>();
+    for (int i = 0; i < transform.childCount; i++) {
+      childs.Add(transform.GetChild(i));
+    }
+  }
+
+  void Update() {
+    for (int i = 0; i < childs.Count; i++) {
+      childs[i].rotation = Camera.main.transform.rotation;
+    }
+  }
+
+  public void AddChild() {
+    Debug.Log("1");
+    childs.Add(transform.GetChild(transform.childCount - 1));
+  }
+}

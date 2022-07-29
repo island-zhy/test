cdc18c617b582610316875c4643aef8e2d5b0815
Kahsolt <kahsolt@qq.com>
2022-07-29 20:07:25 +0800

    add dummy footstep sound effect

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,39 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	[SerializeField]
-	private float _speed = 3f;
+	private float speed = 3f;
 
-	private bool m_ifUpFloor = false;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		float inputX = Input.GetAxis("Horizontal");
-		float inputZ = Input.GetAxis("Vertical");
-		this.GetComponent<Rigidbody>().velocity = new Vector3(inputX, 0, inputZ) * _speed;
-	}
+	private bool m_IfUpFloor = false;
+
+  // 是否处于移动状态
+  private bool isMoving;
+  // 移动音效素材
+  public AudioSource sfx_move;
+
+  // Use this for initialization
+  void Start () {
+    sfx_move = GetComponent<AudioSource>();
+    sfx_move.loop = true;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    float inputX = Input.GetAxis("Horizontal");
+    float inputZ = Input.GetAxis("Vertical");
+    Vector3 move = new Vector3(inputX, 0, inputZ);
+    GetComponent<Rigidbody>().velocity = move * speed;
+
+    if (move != Vector3.zero && !isMoving)
+    {
+      isMoving = true;
+      if (!sfx_move.isPlaying) { sfx_move.Play(); }
+    }
+    else if (move == Vector3.zero && isMoving)
+    {
+      isMoving = false;
+      if (sfx_move.isPlaying) { sfx_move.Stop(); }
+    }
+  }
 
 }

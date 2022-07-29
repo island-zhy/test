fe66b641f7099cb96f2645ae8bd59d6e761f2848
island <zhanghaoyu@hypergryph.com>
2022-07-29 20:07:20 +0800

    push FSM State
    push navigate state

---
+++ Assets/Script/EnemyAi/IStateNavigate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IStateNavigate : IState {
+
+	private FSM m_FSMManager;
+	private EnemyParameter m_enemyParameter;
+
+	public IStateNavigate(FSM fSMManager) {
+    m_FSMManager = fSMManager;
+    m_enemyParameter = fSMManager.m_enemyParameter;
+  }
+
+  public void OnEnter() {
+  
+  }
+
+  public void OnUpdate() {
+  
+  }
+
+  public void OnExit() {
+  
+  }
+}
